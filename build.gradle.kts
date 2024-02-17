b9f00863ea7389cf3ac0e314cbb5c551cebe382d
ramamimu <ahm.f.ramadhani@gmail.com>
2024-02-18 06:05:05 +0700

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,8 @@ tasks.withType<Test> {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.18")
+    testImplementation("o" +
+            "rg.spekframework.spek2:spek-dsl-jvm:2.0.18")
     testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.18")
     testRuntimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.6.10")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
